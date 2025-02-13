df64302abd05c6d5964c84cdc1730f901f285bd9
Anderson Pereira da Silva <andersonpds17042000@gmail.com>
2025-02-13 16:45:32 -0300

    Corrigindo arquivos ignorados

---
+++ backend/app.py
@@ -0,0 +1,9 @@
+print('SABOR EXPRESS')
+
+print('1. cadastrar restaurante')
+print('2. Listar restaurante')
+print('3. Ativar restaurante')
+print('4. Sair\n')
+
+solicitar_comando=int(input('Escolha uma opção: '))
+print(f'Você esolheu {solicitar_comando}')
